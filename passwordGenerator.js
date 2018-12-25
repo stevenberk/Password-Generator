ab4674a0b5d57b7514ddbe736107b24a30322771
Steven Berk <steven.e.berk@gmail.com>
2018-12-25 12:00:49 -0500

    basic principle works but need to include the other variables

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -6,6 +6,7 @@ const readline = require('readline').createInterface({
   let numberOfCapitalLetters = 0;
   let numberOfSpecialCharacters = 0;
   let numberOfNumbers = 0;
+  let passwordLength = 0;
   
   readline.question('How many capital letters does your password need?', (number) => {
     if (parseInt(number, 10)){
@@ -35,6 +36,20 @@ let numberOfNumbersQuestion = () =>{
     readline.question('How many numberical characters does your password need?', (number) => {
         if (parseInt(number, 10)){
             numberOfNumbers += parseInt(number, 10)
+            passwordLengthQuestion()
+        }
+        else{
+            console.log("That is not a valid input")
+        }
+        // readline.close()
+      })
+}
+
+let passwordLengthQuestion = () =>{
+    readline.question('How long does your password need to be?', (number) => {
+        if (parseInt(number, 10)){
+            passwordLength += parseInt(number, 10)
+            runGenerator()
         }
         else{
             console.log("That is not a valid input")
@@ -43,34 +58,33 @@ let numberOfNumbersQuestion = () =>{
       })
 }
 
-
   alphabeticalLetters = {
-    1: "a",
-    2: "b",
-    3: "c",
-    4: "d",
-    5: "e",
-    6: "f",
-    7: "g",
-    8: "h",
-    9: "i",
-    10: "j",
-    11: "k",
-    12: "l",
-    13: "m",
-    14: "n",
-    15: "o",
-    16: "p",
-    17: "q",
-    18: "r",
-    19: "s",
-    20: "t",
-    21: "u",
-    22: "v",
-    23: "w",
-    24: "x",
-    25: "y",
-    26: "z"
+    0: "a",
+    1: "b",
+    2: "c",
+    3: "d",
+    4: "e",
+    5: "f",
+    6: "g",
+    7: "h",
+    8: "i",
+    9: "j",
+    10: "k",
+    11: "l",
+    12: "m",
+    13: "n",
+    14: "o",
+    15: "p",
+    16: "q",
+    17: "r",
+    18: "s",
+    19: "t",
+    20: "u",
+    21: "v",
+    22: "w",
+    23: "x",
+    24: "y",
+    25: "z"
     }
 
     numericalDigits = {
@@ -97,4 +111,20 @@ let numberOfNumbersQuestion = () =>{
         8: "*",
         9: "?",
         10: "."
+    }
+
+let runGenerator = () =>{
+    let runningPassword = ""
+    let numberArray = []
+    for(let i = 0; i < numberOfCapitalLetters; i++){
+        function getRandomInt(max) {  
+            numberArray.push(Math.floor(Math.random() * Math.floor(max)));
         }
+        getRandomInt(26)
+        
+        
+    }
+    numberArray.map(x => runningPassword += alphabeticalLetters[x].toUpperCase())
+    console.log(runningPassword)
+    
+}
